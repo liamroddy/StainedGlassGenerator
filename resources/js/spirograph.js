6f304d54710443478836c28881f6a75cdf1bba9c
Liam <sgt.p2pper@gmail.com>
2021-07-08 10:00:07 +0100

    Temp push Webex Variant

--- resources/js/spirograph.js
+++ resources/js/spirograph.js
@@ -1,1 +1,137 @@
-import{CurveDrawer}from"./curveDrawer.js";export class SpiroGraph{constructor(){this.colourOffset=Math.floor(3*Math.random()),this.blendModeOffset=Math.floor(3*Math.random()),this.tightness=randomInteger(-3,3),this.colourOffset=Math.floor(3*Math.random()),this.blendModeOffset=Math.floor(2*Math.random()),this.subSineAngleChange=randomInteger(4,6)*randomSign(),this.initialSineAngleChange=.04,this.initialSineAngle=0,this.numSines=randomInteger(5,6),this.renderCutoff=0,this.initialSineScale=width/2/3.5,this.lineThickness=1,this.maxRotations=1.1,this.subSineScale=randomReal(.5,.9),this.colour=color(0,0,0,3),this.sines,this.sineLines,this.angle=0,this.setup()}setup(){this.sines=new Array(this.numSines),this.sineLines=new Array(this.numSines);for(let i=0;i<this.sines.length;i++)this.sines[i]=this.angle,this.sineLines[i]=new CurveDrawer,colorMode(RGB),this.sineLines[i].line.colour=this.colour,this.sineLines[i].line.skipAmount=8,this.sineLines[i].line.lineThickness=this.lineThickness,this.sineLines[i].line.maxLength=100;curveTightness(this.tightness)}update(i,e,s,n){-1==s&&(s=this.scale),-1==n&&(n=this.drawingSpeed);let t=this.initialSineScale,h=this.initialSineAngleChange,o=s;if(this.sines[1]<2*Math.PI*this.maxRotations||0==this.maxRotations)for(var r=1;r<this.sines.length;r++){this.sines[r]+=h*n;let s=i+Math.sin(this.sines[r])*t*o,a=e+Math.cos(this.sines[r])*t*o,f=2;(this.blendModeOffset+r)%2==0?(f=2,blendMode(MULTIPLY)):(this.blendModeOffset+r)%2==1&&(f=3,blendMode(ADD));let d=255;if((this.colourOffset+r)%3==0?this.sineLines[r].line.fillColour=color(d,0,0,f):(this.colourOffset+r)%3==1?this.sineLines[r].line.fillColour=color(0,d,0,f):(this.colourOffset+r)%3==2&&(this.sineLines[r].line.fillColour=color(0,0,d,f)),r>=this.renderCutoff)for(var l=0;l<5;l++)this.sineLines[r].addPoint(s,a);i=s,e=a,t=this.initialSineScale*this.subSineScale*(1/r),h*=this.subSineAngleChange}}}function randomInteger(i,e){return i+Math.floor(Math.random()*(e-i+1))}function randomReal(i,e){return map(Math.random(),0,1,i,e)}function randomSign(){return Math.random()>.5?1:-1}
+import { CurveDrawer } from "./curveDrawer.js"
+
+export class SpiroGraph {
+	constructor() {
+        // object variables
+        this.colourOffset = Math.floor(Math.random() * 3) // determines if red, green or blue gets drawn first
+        this.blendModeOffset  = randomInteger(0, 3) // determines what order the blend modes are used in
+        this.tightness = 0//randomInteger(-1, 1) // how curvy/sharp the pattern's curves are
+        this.colourOffset = randomInteger(0, 2)	
+        this.blendModeOffset = randomInteger(0, 1)
+        this.subSineAngleChange = randomInteger(4, 6) * randomSign() 
+        this.initialSineAngleChange = 0.02
+        this.initialSineAngle = 0
+        this.numSines = randomInteger(4, 5)
+        this.renderCutoff = 0
+        this.initialSineScale = (width/(2))/(3.5)
+        this.lineThickness = 2;
+        this.maxRotations = 1.3
+        this.subSineScale = randomReal(0.7, 0.9)
+
+        this.colour = color(22, 177, 224, 10); // line colour
+        this.sines; // an array to hold all the current angles
+        this.sineLines;
+
+        this.setup();
+    }
+	
+	setup()
+	{
+		// setup
+        this.sines = new Array(this.numSines);
+        this.sineLines = new Array(this.numSines);
+
+        for (let i = 0; i<this.sines.length; i++) {
+            this.sines[i] = 0; // intitial angle/period for each sine
+            
+            this.sineLines[i] = new CurveDrawer();
+            this.sineLines[i].line.colour = this.colour
+            this.sineLines[i].line.skipAmount = 8;
+            this.sineLines[i].line.lineThickness = this.lineThickness; 		
+            this.sineLines[i].line.maxLength = 100;	// max number of points per curve
+        }
+
+        curveTightness( this.tightness )
+	}
+    
+    update(x, y, scale, drawingSpeed)
+    {
+        if (scale == -1)
+            scale = this.scale
+        if (drawingSpeed == -1)
+            drawingSpeed = this.drawingSpeed
+        
+        // initial setup
+        let currentSineScale = this.initialSineScale;
+        let currentSineAngleChange = this.initialSineAngleChange;   
+
+        let overallDrawingScale = scale;
+
+        if ( (this.sines[1] < (2 * Math.PI * this.maxRotations) ) || this.maxRotations == 0  )
+        {
+            for (var i=1; i < this.sines.length; i++)
+            {						
+                this.sines[i] += currentSineAngleChange * drawingSpeed;
+                let sineX = x + ( Math.sin(this.sines[i]) * currentSineScale * overallDrawingScale);
+                let sineY = y + ( Math.cos(this.sines[i]) * currentSineScale * overallDrawingScale );
+              
+
+                // let layerAlpha // different blend modes work better at different opacity levels                
+                
+                // if ((this.blendModeOffset + i) % 3 == 0)
+                //     {
+                //         layerAlpha = 2
+                //         blendMode( MULTIPLY )
+                //     }
+                // else if ((this.blendModeOffset + i) % 3 == 1)
+                //     {
+                //         layerAlpha = 3
+                //         blendMode( ADD )
+                //     }
+                // else if ((this.blendModeOffset + i) % 3 == 2)
+                //     {
+                //         layerAlpha = 2
+                //         blendMode( BLEND )
+                //     }
+
+                // let val = 255
+            
+                // if ((this.colourOffset + i) % 3 == 0)
+                //     {
+                //         this.sineLines[i].line.fillColour = color(val, 0, 0, layerAlpha)
+                //     }
+                // else if ((this.colourOffset + i) % 3 == 1)
+                //     {
+                //         this.sineLines[i].line.fillColour = color(0, val, 0, layerAlpha)
+                //     }
+                // else if ((this.colourOffset + i) % 3 == 2)
+                //     {
+                //         this.sineLines[i].line.fillColour = color(0, 0, val, layerAlpha)
+                //     }
+                
+                if (i >= this.renderCutoff)
+                {
+                    //for (var renders=0; renders < 5; renders++)
+                    //{
+                        this.sineLines[i].addPoint(sineX, sineY);	
+                    //}
+                }	
+
+                x = sineX;
+                y = sineY;
+
+                currentSineScale = (this.initialSineScale * this.subSineScale * (1/(i)))
+                currentSineAngleChange *= this.subSineAngleChange
+            }
+        }          
+    }
+}
+
+// Randomisation helper functions:
+
+function randomInteger(min, max)
+{
+	let me = min + Math.floor(Math.random() * (max - min + 1))
+	return me 
+}
+
+function randomReal(min, max)
+{
+	let me = map(Math.random(), 0, 1, min, max)
+	return me 
+}
+
+function randomSign()
+{
+	return (Math.random() > 0.5 ? 1 : -1 ) 
+}
